dda442e848fd003a1af716d96c1c330f795f355d
Andreas Schwier <andreas.schwier@cardcontact.de>
2013-05-23 09:17:18 +0200

    Adding missing files

---
+++ src/ctccid/dump.c
@@ -0,0 +1,84 @@
+/*
+ *  ---------
+ * |.**> <**.|  CardContact
+ * |*       *|  Software & System Consulting
+ * |*       *|  Minden, Germany
+ * |.**> <**.|  Copyright (c) 2013. All rights reserved
+ *  ---------
+ *
+ * See file LICENSE for details on licensing
+ *
+ * Abstract :       Set of utility functions
+ *
+ * Author :         Frank Thater
+ *
+ * Last modified:   2013-05-07
+ *
+ *****************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "dump.h"
+
+
+
+#ifdef DEBUG
+/*
+ * Dump the memory pointed to by <ptr>
+ *
+ */
+void ctccidDump(void *_ptr, int len)
+{
+	unsigned char *ptr = (unsigned char *)_ptr;
+	int i;
+
+#ifdef DEBUG
+	static char *MinStack = (char *)-1;
+	static char *MaxStack; /* = 0; */
+	if (MinStack > (char *)&ptr)
+		MinStack = (char *)&ptr;
+	if (MaxStack < (char *)&ptr)
+		MaxStack = (char *)&ptr;
+	printf("Dump(%p, %d) stack used so far: %d", ptr, len, MaxStack - MinStack);
+#else
+	printf("Dump(%p, %d)", ptr, len);
+#endif
+
+	for (i = 0; i < len; i += 16) {
+		int i1 = i + 16;
+		int i2 = i1;
+		int j;
+
+		if (i1 > len) {
+			i1 = len;
+		}
+
+		if (i % 16 == 0) {
+			printf("\n  %04x: ", (char*)i);
+		}
+
+		for (j = i; j < i1; j++) {
+			printf("%02x ", ptr[j]);
+		}
+
+		for (     ; j < i2; j++) {
+			printf("   ");
+		}
+
+		printf(" ");
+
+		for (j = i; j < i1; j++) {
+			unsigned char ch = ptr[j];
+
+			if (!isprint(ch)) {
+				ch = '.';
+			}
+
+			printf("%c", ch);
+		}
+	}
+
+	printf("\n");
+}
+#endif
---
+++ src/ctccid/dump.h
@@ -0,0 +1,32 @@
+/*
+ *  ---------
+ * |.**> <**.|  CardContact
+ * |*       *|  Software & System Consulting
+ * |*       *|  Minden, Germany
+ * |.**> <**.|  Copyright (c) 2013. All rights reserved
+ *  ---------
+ *
+ * See file LICENSE for details on licensing
+ *
+ * Abstract :       Simple abstraction layer for USB devices using libusb
+ *
+ * Author :         Frank Thater
+ *
+ * Last modified:   2013-05-07
+ *
+ *****************************************************************************/
+
+#ifndef __DUMP_H__                     /* Prevent from including twice      */
+#define __DUMP_H__
+
+#ifdef __cplusplus                      /* Support for C++ compiler          */
+extern "C" {
+#endif
+
+void ctccidDump(void *ptr, int len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
