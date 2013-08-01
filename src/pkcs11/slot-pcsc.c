CardContact/sc-hsm-embedded

commit 80ab13d601db316972a9c17f5e876e21246b7e87

PKCS11: Fixed newToken() API

src/pkcs11/slot-pcsc.c
@@ -427,7 +427,7 @@ static int checkForNewPCSCToken(struct p11Slot_t *slot)
 		FUNC_FAILS(CKR_DEVICE_ERROR, pcsc_error_to_string(rc));
 	}
 
-	rc = newToken(slot, &ptoken);
+	rc = newToken(slot, atr, strlen, &ptoken);
 
 	if (rc != CKR_OK) {
 		FUNC_FAILS(rc, "newToken() failed");

commit 9981f1c192ed33f38cd7b3aa32d4bed157ca9c4a

pcsc: Fixed typo introduced in 80ab13d601db316972a9c17f5e876e21246b7e87

src/pkcs11/slot-pcsc.c
@@ -428,7 +428,7 @@ static int checkForNewPCSCToken(struct p11Slot_t *slot)
 		FUNC_FAILS(CKR_DEVICE_ERROR, pcsc_error_to_string(rc));
 	}
 
-	rc = newToken(slot, atr, strlen, &ptoken);
+	rc = newToken(slot, atr, atrlen, &ptoken);
 
 	if (rc != CKR_OK) {
 		FUNC_FAILS(rc, "newToken() failed");