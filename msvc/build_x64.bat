6a1bfbb5b4f6a356ef295b5654472d2be08c7759
Frank Thater <frank.thater@cardcontact.de>
2013-09-19 12:42:14 +0200

    PKCS11: Added batch files for integration build

---
+++ msvc/build_x64.bat
@@ -0,0 +1,9 @@
+cd /d %~dp0
+
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd"
+
+call "setenv" /x64
+
+msbuild sc-hsm-pcsc-pkcs11.vcxproj /property:Configuration=Release;Platform=x64
+
+msbuild sc-hsm-pcsc-pkcs11-test.vcxproj /property:Configuration=Debug;Platform=x64
---
+++ msvc/build_x86.bat
@@ -0,0 +1,9 @@
+cd /d %~dp0
+
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd"
+
+call "setenv" /x86
+
+msbuild sc-hsm-pcsc-pkcs11.vcxproj /property:Configuration=Release;Platform=Win32
+
+msbuild sc-hsm-pcsc-pkcs11-test.vcxproj /property:Configuration=Debug;Platform=Win32
