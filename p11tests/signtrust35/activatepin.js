444e5544a737113d97eb6c3f144254065e7d6239
Andreas Schwier <andreas.schwier@cardcontact.de>
2013-12-18 11:39:08 +0100

    Added some debugging output

--- p11tests/signtrust35/activatepin.js
+++ p11tests/signtrust35/activatepin.js
@@ -1,15 +1,28 @@
 
 
-var tupin = new ByteString("2599193FFFFFFFFF", HEX);
+var tupin = new ByteString("2515224FFFFFFFFF", HEX);
 var upin  = new ByteString("26123456FFFFFFFF", HEX);
 
-var tspin = new ByteString("2568826FFFFFFFFF", HEX);
+var tspin = new ByteString("2576224FFFFFFFFF", HEX);
 var spin  = new ByteString("26123456FFFFFFFF", HEX);
 
 
 var card = new Card(_scsh3.reader);
-card.sendApdu(0x00, 0x24, 0x00, 0x06, tupin.concat(upin), [0x9000]);
+card.reset(Card.RESET_COLD);
+
+var ef = new CardFile(card, ":2F02");
+print(ef.readBinary());
+
+var ef = new CardFile(card, ":0013");
+var rec = ef.readRecord(17);
+rec = new ASN1(0x30, rec).getBytes();
+print(new ASN1(rec));
+
+card.sendApdu(0x00, 0x20, 0x00, 0x06);
+
+//card.sendApdu(0x00, 0x24, 0x00, 0x06, tupin.concat(upin), [0x9000]);
 
 var df = new CardFile(card, "#D27600006601");
 
-df.sendApdu(0x00, 0x24, 0x00, 0x81, tspin.concat(spin), [0x9000]);
+df.sendApdu(0x00, 0x20, 0x00, 0x81);
+//df.sendApdu(0x00, 0x24, 0x00, 0x81, tspin.concat(spin), [0x9000]);
