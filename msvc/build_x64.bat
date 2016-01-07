140b1621ae3897c09b0fb48573c06718bd03d85a
Andreas Schwier <andreas.schwier@cardcontact.de>
2016-01-07 11:28:37 +0100

    Add MSVC project files and libcurl for Windows

---
+++ msvc/build_x64.bat
@@ -0,0 +1,13 @@
+cd /d %~dp0
+
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd"
+
+call "setenv" /x64
+
+msbuild sc-hsm-pkcs11-pcsc\sc-hsm-pkcs11-pcsc.vcxproj /property:Configuration=Release;Platform=x64
+
+msbuild sc-hsm-pkcs11-pcsc\sc-hsm-pkcs11-pcsc.vcxproj /property:Configuration=Debug;Platform=x64
+
+msbuild sc-hsm-pkcs11-test\sc-hsm-pkcs11-test.vcxproj /property:Configuration=Release;Platform=x64
+
+msbuild sc-hsm-pkcs11-test\sc-hsm-pkcs11-test.vcxproj /property:Configuration=Debug;Platform=x64
---
+++ msvc/build_x86.bat
@@ -0,0 +1,17 @@
+cd /d %~dp0
+
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd"
+
+call "setenv" /x86
+
+msbuild sc-hsm-pkcs11-pcsc\sc-hsm-pkcs11-pcsc.vcxproj /property:Configuration=Release;Platform=Win32
+
+msbuild sc-hsm-pkcs11-pcsc\sc-hsm-pkcs11-pcsc.vcxproj /property:Configuration=Debug;Platform=Win32
+
+msbuild sc-hsm-pkcs11-test\sc-hsm-pkcs11-test.vcxproj /property:Configuration=Release;Platform=Win32
+
+msbuild sc-hsm-pkcs11-test\sc-hsm-pkcs11-test.vcxproj /property:Configuration=Debug;Platform=Win32
+
+msbuild ram-client\ram-client.vcxproj /property:Configuration=Release;Platform=Win32
+
+msbuild ram-client\ram-client.vcxproj /property:Configuration=Debug;Platform=Win32
