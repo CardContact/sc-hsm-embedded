27c789bae6c076383874116515f2ed61e84d8439
Andreas Schwier <andreas.schwier@cardcontact.de>
2017-07-28 14:52:06 +0100

    Improve error reporting

--- src/tests/sc-hsm-minidriver-test.c
+++ src/tests/sc-hsm-minidriver-test.c
@@ -42,36 +42,73 @@ static int testsfailed = 0;
 static char *reader = NULL;
 
 
-#ifndef _WIN32
-
-#include <unistd.h>
-#include <dlfcn.h>
-#define LIB_HANDLE void*
-#define P11LIBNAME "/usr/local/lib/libsc-hsm-pkcs11.so"
-
-#else
-
 #include <windows.h>
 #include <malloc.h>
-#define LIB_HANDLE HMODULE
-#define P11LIBNAME "sc-hsm-pkcs11.dll"
-
-#define dlopen(fn, flag) LoadLibrary(fn)
-#define dlclose(h) FreeLibrary(h)
-#define dlsym(h, n) GetProcAddress(h, n)
-
-char* dlerror()
-{
-	char* msg = "UNKNOWN";
-	DWORD le = GetLastError();
-	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, 0, le, 0, (char*)&msg, 0, 0);
-	return msg;
-}
-#endif /* _WIN32 */
-
-
 #include <cardmod.h>
 
+
+
+char *SystemErrorMsg(DWORD rc)
+{
+	char *msg = "UNKNOWN";
+
+	if (!FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, 0, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0)) {
+		FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, GetModuleHandle("crypt32.dll"), rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	}
+	return msg;
+}
+
+
+
+char *NTSTATUSErrorMsg(NTSTATUS nts)
+{
+	char *msg = "UNKNOWN";
+
+	HMODULE hmod = GetModuleHandle("ntdll.dll");
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, hmod, nts, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	return msg;
+}
+
+
+
+char *SECURITY_STATUSErrorMsg(SECURITY_STATUS secstat)
+{
+	char *msg = "UNKNOWN";
+
+	HMODULE hmod = GetModuleHandle("crypt32.dll");
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, hmod, secstat, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	return msg;
+}
+
+
+
+/*
+char *ErrorMsg(DWORD rc)
+{
+	char *msg = "UNKNOWN";
+	HMODULE hmod;
+
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, 0, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	hmod = GetModuleHandle("bcrypt.dll");
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, hmod, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	hmod = GetModuleHandle("ncrypt.dll");
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, hmod, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	hmod = GetModuleHandle("crypt32.dll");
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, hmod, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	hmod = GetModuleHandle("ntdll.dll");
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, hmod, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	hmod = GetModuleHandle("kernel32.dll");
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, hmod, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	hmod = GetModuleHandle("KernelBase.dll");
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, hmod, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	hmod = GetModuleHandle("msvcrt.dll");
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, hmod, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+	hmod = GetModuleHandle("cryptbase.dll");
+	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_HMODULE, hmod, rc, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char*)&msg, 0, 0);
+
+	return msg;
+}
+*/
 
 
 static char *verdict(int condition) {
@@ -116,7 +153,7 @@ int testSignRSA(NCRYPT_KEY_HANDLE hKey, DWORD padding, LPCWSTR hashAlg )
 	PCCERT_CONTEXT certctx;
 	NTSTATUS ntstat;
 	unsigned char cert[4096],hash[64],signature[256],pubkeyblob[1024];
-	DWORD dwlen,hashlen;
+	DWORD dwrc,dwlen,hashlen;
 
 	printf(" RSA signing with %S and %s padding", hashAlg, (padding == BCRYPT_PAD_PKCS1 ? "V1.5" : "PSS"));
 
@@ -150,33 +187,35 @@ int testSignRSA(NCRYPT_KEY_HANDLE hKey, DWORD padding, LPCWSTR hashAlg )
 	secstat = NCryptExportKey(hKey, 0, BCRYPT_RSAPUBLIC_BLOB, 0, pubkeyblob, sizeof(pubkeyblob), &dwlen, 0);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("\nNCryptExportKey failed: %ld\n", secstat);
+		printf("\nNCryptExportKey failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
 	ntstat = BCryptOpenAlgorithmProvider(&hSignAlg, BCRYPT_RSA_ALGORITHM, NULL, 0);
+
 	if (ntstat != ERROR_SUCCESS) {
-		printf("\nBCryptOpenAlgorithmProvider failed: %ld\n", ntstat);
+		printf("\nBCryptOpenAlgorithmProvider failed: %08lx %s", ntstat, NTSTATUSErrorMsg(ntstat));
 		return -1;
 	}
 
 	ntstat = BCryptImportKeyPair(hSignAlg, 0, BCRYPT_RSAPUBLIC_BLOB, &hPubKey, pubkeyblob, dwlen, 0);
+
 	if (ntstat != ERROR_SUCCESS) {
-		printf("\nBCryptImportKeyPair failed: %ld\n", ntstat);
+		printf("\nBCryptImportKeyPair failed: %08lx %s", ntstat, NTSTATUSErrorMsg(ntstat));
 		return -1;
 	}
 
 	secstat = NCryptSignHash(hKey, paddingInfo, hash, hashlen, signature, sizeof(signature), &dwlen, padding);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("\nNCryptSignHash failed: %ld\n", secstat);
+		printf("\nNCryptSignHash failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
 	ntstat = BCryptVerifySignature(hPubKey, paddingInfo, hash, hashlen, signature, dwlen, padding);
 
 	if (ntstat != ERROR_SUCCESS) {
-		printf("\nBCryptVerifySignature failed: %ld\n", ntstat);
+		printf("\nBCryptVerifySignature failed: %08lx %s", ntstat, NTSTATUSErrorMsg(ntstat));
 		return -1;
 	}
 
@@ -187,32 +226,35 @@ int testSignRSA(NCRYPT_KEY_HANDLE hKey, DWORD padding, LPCWSTR hashAlg )
 	secstat = NCryptGetProperty(hKey, NCRYPT_CERTIFICATE_PROPERTY, cert, sizeof(cert), &dwlen, 0);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("\nNCryptGetProperty failed: %ld\n", secstat);
+		printf("\nNCryptGetProperty failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
 	certctx = CertCreateCertificateContext(X509_ASN_ENCODING, cert, dwlen);
+
 	if (certctx == NULL) {
-		printf("\nCertCreateCertificateContext failed\n");
+		dwrc = GetLastError();
+		printf("\nCertCreateCertificateContext failed: %04x %s\n", dwrc, SystemErrorMsg(dwrc));
 		return -1;
 	}
 
 	if (!CryptImportPublicKeyInfoEx2(X509_ASN_ENCODING, &certctx->pCertInfo->SubjectPublicKeyInfo, 0, NULL, &hPubKey)) {
-		printf("\nCryptImportPublicKeyInfoEx2 failed\n");
+		dwrc = GetLastError();
+		printf("\nCryptImportPublicKeyInfoEx2 failed: %04x %s\n", dwrc, SystemErrorMsg(dwrc));
 		return -1;
 	}
 
 	secstat = NCryptSignHash(hKey, paddingInfo, hash, hashlen, signature, sizeof(signature), &dwlen, padding);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("\nNCryptSignHash failed: %ld\n", secstat);
+		printf("\nNCryptSignHash failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
 	ntstat = BCryptVerifySignature(hPubKey, paddingInfo, hash, hashlen, signature, dwlen, padding);
 
 	if (ntstat != ERROR_SUCCESS) {
-		printf("\nBCryptVerifySignature failed: %ld\n", ntstat);
+		printf("\nBCryptVerifySignature failed: %08lx %s", ntstat, NTSTATUSErrorMsg(ntstat));
 		return -1;
 	}
 
@@ -232,7 +274,7 @@ int testSignECDSA(NCRYPT_KEY_HANDLE hKey, LPCWSTR hashAlg )
 //	BCRYPT_ALG_HANDLE hSignAlg;
 	NTSTATUS ntstat;
 	unsigned char cert[4096],hash[64],signature[256];	// ,pubkeyblob[1024];
-	DWORD dwlen,hashlen;
+	DWORD dwrc,dwlen,hashlen;
 
 	printf(" ECDSA with %S", hashAlg);
 
@@ -256,7 +298,7 @@ int testSignECDSA(NCRYPT_KEY_HANDLE hKey, LPCWSTR hashAlg )
 	secstat = NCryptExportKey(hKey, 0, BCRYPT_ECCPUBLIC_BLOB, 0, pubkeyblob, sizeof(pubkeyblob), &dwlen, 0);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("\nNCryptExportKey failed: %ld\n", secstat);
+		printf("\nNCryptExportKey failed: %08lx %s\n", ntstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
@@ -275,7 +317,7 @@ int testSignECDSA(NCRYPT_KEY_HANDLE hKey, LPCWSTR hashAlg )
 	secstat = NCryptSignHash(hKey, NULL, hash, hashlen, signature, dwlen, &dwlen, 0);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("\nNCryptSignHash failed: %ld\n", secstat);
+		printf("\nNCryptSignHash failed: %08lx %s\n", ntstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
@@ -293,32 +335,35 @@ int testSignECDSA(NCRYPT_KEY_HANDLE hKey, LPCWSTR hashAlg )
 	secstat = NCryptGetProperty(hKey, NCRYPT_CERTIFICATE_PROPERTY, cert, sizeof(cert), &dwlen, 0);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("\nNCryptGetProperty failed: %ld\n", secstat);
+		printf("\nNCryptGetProperty failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
 	certctx = CertCreateCertificateContext(X509_ASN_ENCODING, cert, dwlen);
+
 	if (certctx == NULL) {
-		printf("\nCertCreateCertificateContext failed\n");
+		dwrc = GetLastError();
+		printf("\nCertCreateCertificateContext failed: %04x %s\n", dwrc, SystemErrorMsg(dwrc));
 		return -1;
 	}
 
 	if (!CryptImportPublicKeyInfoEx2(X509_ASN_ENCODING, &certctx->pCertInfo->SubjectPublicKeyInfo, 0, NULL, &hPubKey)) {
-		printf("\nCryptImportPublicKeyInfoEx2 failed\n");
+		dwrc = GetLastError();
+		printf("\nCryptImportPublicKeyInfoEx2 failed: %04x %s\n", dwrc, SystemErrorMsg(dwrc));
 		return -1;
 	}
 
 	secstat = NCryptSignHash(hKey, NULL, hash, hashlen, signature, dwlen, &dwlen, 0);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("\nNCryptSignHash failed: %ld\n", secstat);
+		printf("\nNCryptSignHash failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
 	ntstat = BCryptVerifySignature(hPubKey, NULL, hash, hashlen, signature, dwlen, 0);
 
 	if (ntstat != ERROR_SUCCESS) {
-		printf("\nBCryptVerifySignature failed: %ld\n", ntstat);
+		printf("\nBCryptVerifySignature failed: %08lx %s", ntstat, NTSTATUSErrorMsg(ntstat));
 		return -1;
 	}
 
@@ -338,7 +383,7 @@ int testDecryptRSA(NCRYPT_KEY_HANDLE hKey, DWORD padding )
 	BCRYPT_ALG_HANDLE hSignAlg;
 	void *paddingInfo;
 	NTSTATUS ntstat;
-	unsigned char secret[64],plain[256],cryptogram[256],pubkeyblob[1024];
+	unsigned char secret[48],plain[256],cryptogram[256],pubkeyblob[1024];
 	DWORD dwlen,secretlen;
 
 	printf(" RSA decryption with %s padding", (padding == BCRYPT_PAD_PKCS1 ? "V1.5" : "OAEP"));
@@ -359,33 +404,35 @@ int testDecryptRSA(NCRYPT_KEY_HANDLE hKey, DWORD padding )
 	secstat = NCryptExportKey(hKey, 0, BCRYPT_RSAPUBLIC_BLOB, 0, pubkeyblob, sizeof(pubkeyblob), &dwlen, 0);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("\nNCryptExportKey failed: %ld\n", secstat);
+		printf("\nNCryptExportKey failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
 	ntstat = BCryptOpenAlgorithmProvider(&hSignAlg, BCRYPT_RSA_ALGORITHM, NULL, 0);
+
 	if (ntstat != ERROR_SUCCESS) {
-		printf("\nBCryptOpenAlgorithmProvider failed: %ld\n", ntstat);
+		printf("\nBCryptOpenAlgorithmProvider failed: %08lx %s", ntstat, NTSTATUSErrorMsg(ntstat));
 		return -1;
 	}
 
 	ntstat = BCryptImportKeyPair(hSignAlg, 0, BCRYPT_RSAPUBLIC_BLOB, &hPubKey, pubkeyblob, dwlen, 0);
+
 	if (ntstat != ERROR_SUCCESS) {
-		printf("\nBCryptImportKeyPair failed: %ld\n", ntstat);
+		printf("\nBCryptImportKeyPair failed: %08lx %s", ntstat, NTSTATUSErrorMsg(ntstat));
 		return -1;
 	}
 
 	ntstat = BCryptEncrypt(hPubKey, secret, secretlen, paddingInfo, NULL, 0, cryptogram, sizeof(cryptogram), &dwlen, padding);
 
 	if (ntstat != ERROR_SUCCESS) {
-		printf("\nBCryptEncrypt failed: %ld\n", ntstat);
+		printf("\nBCryptEncrypt failed: %08lx %s", ntstat, NTSTATUSErrorMsg(ntstat));
 		return -1;
 	}
 
 	secstat = NCryptDecrypt(hKey, cryptogram, dwlen, paddingInfo, plain, sizeof(plain), &dwlen, padding);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("\nNCryptExportKey failed: %ld\n", secstat);
+		printf("\nNCryptExportKey failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
@@ -415,14 +462,14 @@ int cryptoTests()
 	secstat = NCryptOpenStorageProvider(&hProvider, MS_SMART_CARD_KEY_STORAGE_PROVIDER, 0);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("NCryptOpenStorageProvider failed: %ld\n", secstat);
+		printf("NCryptOpenStorageProvider failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
 	secstat = NCryptEnumAlgorithms(hProvider, NCRYPT_CIPHER_OPERATION|NCRYPT_HASH_OPERATION|NCRYPT_ASYMMETRIC_ENCRYPTION_OPERATION|NCRYPT_SECRET_AGREEMENT_OPERATION, &dwlen, &algos, 0);
 
 	if (secstat != ERROR_SUCCESS) {
-		printf("NCryptEnumAlgorithms failed: %ld\n", secstat);
+		printf("NCryptEnumAlgorithms failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 		return -1;
 	}
 
@@ -444,7 +491,7 @@ int cryptoTests()
 		secstat = NCryptOpenKey(hProvider, &hKey, keyName->pszName, 0, 0);
 
 		if (secstat != ERROR_SUCCESS) {
-			printf("Failed: %ld\n", secstat);
+			printf("NCryptOpenKey failed: %08lx %s\n", secstat, SECURITY_STATUSErrorMsg(secstat));
 			return -1;
 		}
 
@@ -540,7 +587,7 @@ int listReaders()
 
 int apiTests(char *reader)
 {
-	LIB_HANDLE dlhandle;
+	HMODULE dlhandle;
 	PFN_CARD_ACQUIRE_CONTEXT pcac;
 	CARD_FREE_SPACE_INFO cardFreeSpaceInfo;
 	CARD_CAPABILITIES cardCapabilities;
@@ -569,14 +616,15 @@ int apiTests(char *reader)
 		exit(1);
 	}
 
-	dlhandle = dlopen("sc-hsm-minidriver.dll", RTLD_NOW);
+	dlhandle = LoadLibrary("sc-hsm-minidriver.dll");
 
 	if (!dlhandle) {
-		printf("dlopen failed with %s\n", dlerror());
+		dwrc = GetLastError();
+		printf("LoadLibrary failed %04x %s\n", dwrc, SystemErrorMsg(dwrc));
 		exit(1);
 	}
 
-	pcac = (PFN_CARD_ACQUIRE_CONTEXT)dlsym(dlhandle, "CardAcquireContext");
+	pcac = (PFN_CARD_ACQUIRE_CONTEXT)GetProcAddress(dlhandle, "CardAcquireContext");
 
 	readernamelen = 0;
 	atrlen = sizeof(atr);
