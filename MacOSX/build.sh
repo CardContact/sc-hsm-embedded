f5d8cf61a0cf01e83d6d55db7aafbef22003ad65
Andreas Schwier <andreas.schwier@cardcontact.de>
2022-06-08 19:20:17 +0200

    Bump to 2.12

--- MacOSX/build.sh
+++ MacOSX/build.sh
@@ -13,4 +13,4 @@ make clean
 make
 cp src/pkcs11/.libs/libsc-hsm-pkcs11.so ${BUILDDIR}/lib/sc-hsm-pkcs11-debug.dylib
 cp src/tests/sc-hsm-pkcs11-test ${BUILDDIR}/bin
-pkgbuild --root build/target --identifier de.cardcontact.sc-hsm-pkcs11 --version 2.11 --install-location /Library sc-hsm-pkcs11.pkg
+pkgbuild --root build/target --identifier de.cardcontact.sc-hsm-pkcs11 --version 2.12 --install-location /Library sc-hsm-pkcs11.pkg
